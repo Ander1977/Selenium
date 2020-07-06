0af718449e8df68859c984b2c9976ffdd92791d8
Ander1977 <kopa.andrej@gmail.com>
2020-07-06 22:47:39 +0300

    Changed test

--- src/test/java/test/java/order/OrderTest.java
+++ src/test/java/test/java/order/OrderTest.java
@@ -1,8 +1,9 @@
 package test.java.order;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -26,6 +27,16 @@ public class OrderTest {
         $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
         $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
         $("[class='Success_successBlock__2L3Cw']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldInputNotCurrent() {
+        $("[data-test-id='name'] input").setValue("Kopatilov Andrey");
+        $("[data-test-id='phone'] input").setValue("+79098765432");
+        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
+        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(By.className("input_invalid")).shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
 }
