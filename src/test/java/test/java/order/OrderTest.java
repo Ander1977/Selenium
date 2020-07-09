3e9a32f9f20a32d83b1d67bc176841b06eae0d8b
Ander1977 <kopa.andrej@gmail.com>
2020-07-09 16:22:14 +0300

    Changed test

--- src/test/java/test/java/order/OrderTest.java
+++ src/test/java/test/java/order/OrderTest.java
@@ -4,8 +4,7 @@ package test.java.order;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.cssValue;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -16,9 +15,9 @@ public class OrderTest {
         open("http://localhost:9999");
         $("[data-test-id='name'] input").setValue("Копатилов Андрей");
         $("[data-test-id='phone'] input").setValue("+79098765432");
-        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[class='Success_successBlock__2L3Cw']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
@@ -26,8 +25,8 @@ public class OrderTest {
         open("http://localhost:9999");
         $("[data-test-id='name'] input").setValue("Kopatilov Andrey");
         $("[data-test-id='phone'] input").setValue("+79098765432");
-        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white']").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Продолжить")).click();
         $(By.className("input_invalid")).shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
         $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
@@ -38,9 +37,8 @@ public class OrderTest {
         $("[data-test-id='name'] input").setValue("Копатилов Андрей");
         $("[data-test-id='phone'] input").setValue("89098765432");
         $("[data-test-id=agreement]").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-        $("[data-test-id='phone']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $$("button").find(exactText("Продолжить")).click();
+        $(By.className("input_invalid")).shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -48,8 +46,9 @@ public class OrderTest {
         open("http://localhost:9999");
         $("[data-test-id='name'] input").setValue("Копатилов Андрей");
         $("[data-test-id='phone'] input").setValue("+79098765432");
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[class='checkbox__text']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id=agreement]").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(By.className("input_invalid")).shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
@@ -57,9 +56,8 @@ public class OrderTest {
         open("http://localhost:9999");
         $("[data-test-id='phone'] input").setValue("+79098765432");
         $("[data-test-id=agreement]").click();
-        $("button").click();
-        $("[data-test-id='name']").shouldHave(text("Поле обязательно для заполнения"));
-        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $$("button").find(exactText("Продолжить")).click();
+        $(By.className("input_invalid")).shouldHave(text("Поле обязательно для заполнения"));
     }
 
 }
